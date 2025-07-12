1bec509873a6da1ea5e0f35d02ad231f45984879
Rob Culliton <rob.culliton@gmail.com>
2025-07-12 09:35:44 -0500

    working tests! nice job robert

--- lua/plugins/rustacean.lua
+++ lua/plugins/rustacean.lua
@@ -9,6 +9,10 @@ return {
 			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
 			vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
 			vim.keymap.set({ 'n', 'v' }, '<leader>a', vim.lsp.buf.code_action, opts)
+			vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, opts)
+			vim.keymap.set("n", "<leader>t", function()
+			  vim.cmd.RustLsp("testables")
+			end, { desc = "Run Rust test" })
 		end
 	})
   end
