f9e3a2d17eb7da34ccd101d34e8b12785878ac89
Rob Culliton <rob.culliton@gmail.com>
2025-07-21 16:14:54 -0500

    [main] use rustaceanvim's existing support for on_attach. also get a basic rust analyzer check on save in place with an explicit command of "check"

--- lua/plugins/rustacean.lua
+++ lua/plugins/rustacean.lua
@@ -3,8 +3,9 @@ return {
   version = '^6', -- Recommended
   lazy = false, -- This plugin is already lazy
   config = function()
-	vim.api.nvim_create_autocmd('LspAttach', {
-		callback = function(ev)
+    vim.g.rustaceanvim = {
+      server = {
+	on_attach = function(client, bufnr)
 			vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
 			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
 			vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
@@ -13,7 +14,16 @@ return {
 			vim.keymap.set("n", "<leader>t", function()
 			  vim.cmd.RustLsp("testables")
 			end, { desc = "Run Rust test" })
-		end
-	})
+    	end,
+
+        settings = {
+          ["rust-analyzer"] = {
+            checkOnSave = {
+              command = "check",
+            },
+          },
+        },
+      },
+    }
   end
 }
