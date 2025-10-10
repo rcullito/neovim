b96924950924bd1dd5f5e81524c3f8123aa4ea37
Rob Culliton <rob.culliton@gmail.com>
2025-10-10 12:37:39 -0500

    readme and debugging shortcuts

--- README.md
+++ README.md
@@ -8,5 +8,19 @@
 ## Testing
 
 ```
-:RustLsp testables
+Space t, which equates to :RustLsp testables
 ```
+
+
+## Debugging
+
+Rustacean.nvim uses nvim-dap for debugging. The standard DAP commands work
+
+```
+Space db, which equates to :lua require('dap').toggle_breakpoint()
+Space dc, which equates to :lua require('dap').continue() ## this will actually run the debugger and ask you to select a target
+Space dt, which equats to terminating the debugging session.
+```
+
+## TODO 
+set keybindings for the above commands. 
--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -1,5 +1,14 @@
   return {
     'mfussenegger/nvim-dap',
+    keys = {
+      { '<leader>db', function() require('dap').toggle_breakpoint() end, desc = 'Toggle breakpoint' },
+      { '<leader>dc', function() require('dap').continue() end, desc = 'Start/Continue debugging' },
+      { '<leader>ds', function() require('dap').step_over() end, desc = 'Step over' },
+      { '<leader>di', function() require('dap').step_into() end, desc = 'Step into' },
+      { '<leader>do', function() require('dap').step_out() end, desc = 'Step out' },
+      { '<leader>dt', function() require('dap').terminate() end, desc = 'Terminate debugging' },
+      { '<leader>dr', function() require('dap').repl.open() end, desc = 'Open debug REPL' },
+    },
     config = function()
 			local dap, dapui = require("dap"), require("dapui")
       dap.listeners.before.attach.dapui_config = function()
